f41ea5915aeddb6368189fab682b5437ae5b55a7
AtharvaCM <necrons999@gmail.com>
2022-11-03 17:36:03 +0530

    searchInput added to page

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,9 +2,47 @@ import type { NextPage } from "next";
 
 // custom components
 import Layout from "@/components/layout";
+import SearchInput from "@/components/search-input";
+import React, { useState } from "react";
+import { useAxios } from "src/hooks/useAxios";
+import Spinner from "@/components/UI/spinner";
 
 const Search: NextPage = () => {
-  return <Layout>Search Page</Layout>;
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [showArticles, setShowArticles] = useState<boolean>(false);
+
+  const { loaded, callAPI, setLoaded } = useAxios();
+
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.currentTarget.value);
+  };
+
+  const handleSearchClick = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    setShowArticles(true);
+    setLoaded(false);
+    const url: string = `https://newsapi.org/v2/everything?q="${searchInput}"&apiKey=${process.env.newsAPIKey}`;
+
+    callAPI(url);
+  };
+
+  return (
+    <Layout>
+      <div className="mx-[10%] pt-10">
+        <div className="mx-auto max-w-[1024px]">
+          {/* Search */}
+          <SearchInput
+            placeholder="Search News Articles..."
+            onChange={handleSearchInputChange}
+            onSearchClick={handleSearchClick}
+          />
+          {/* Display results */}
+          {showArticles && !loaded && <Spinner />}
+          {showArticles && loaded && <p>Results loaded</p>}
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Search;
